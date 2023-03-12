23cdd3d77907c97b5a217f689208641da5db79b5
twizy <twizalain@gmail.com>
2023-03-12 18:36:49 +0200

    Properties

---
+++ app/src/main/java/com/example/foodshipping/ArticleClass.java
@@ -0,0 +1,41 @@
+package com.example.foodshipping;
+
+public class ArticleClass {
+
+    String productname, productunit, productprice, productKey;
+
+    public ArticleClass() {
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getProductunit() {
+        return productunit;
+    }
+
+    public void setProductunit(String productunit) {
+        this.productunit = productunit;
+    }
+
+    public String getProductprice() {
+        return productprice;
+    }
+
+    public void setProductprice(String productprice) {
+        this.productprice = productprice;
+    }
+
+    public String getProductKey() {
+        return productKey;
+    }
+
+    public void setProductKey(String productKey) {
+        this.productKey = productKey;
+    }
+}
