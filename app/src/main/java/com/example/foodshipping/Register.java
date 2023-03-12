bee21c9b0ab71f7b6e1bdc9b82a76da38bb90800
twizy <twizalain@gmail.com>
2023-03-12 12:12:29 +0200

    google services and gradle settings

--- app/src/main/java/com/example/foodshipping/Register.java
+++ app/src/main/java/com/example/foodshipping/Register.java
@@ -3,8 +3,35 @@ package com.example.foodshipping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.database.DatabaseReference;
 public class Register extends AppCompatActivity {
+
+//    private EditText usernameEdx, passwordEdx;
+//    private Button signupBtn, loginBtn;
+//    private FirebaseAuth auth;
+//    private ProgressDialog progressDialog;
+//    private DatabaseReference rootRef;
+//    private FirebaseUser user;
+//    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
