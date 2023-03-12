966c23bc96c6f89bb9e4728339eb7a65e077ec08
twizy <twizalain@gmail.com>
2023-03-12 17:25:43 +0200

    Article code

--- app/src/main/java/com/example/foodshipping/Articles.java
+++ app/src/main/java/com/example/foodshipping/Articles.java
@@ -2,13 +2,61 @@ package com.example.foodshipping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Articles extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<ProductsClass> list;
+    private ArrayAdapter<ProductsClass> adapter;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
+    private ProgressDialog progressDialog = null;
+    private String stringKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.article_toolbar);
+        toolbar.setTitle("Product list");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+
+        progressDialog = new ProgressDialog(Articles.this);
+        progressDialog.setMessage("loading...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        list = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.listview);
     }
 }
