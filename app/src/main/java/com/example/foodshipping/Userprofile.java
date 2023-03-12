78c651851559b96afa65a4ed39dea4c3adc1f207
twizy <twizalain@gmail.com>
2023-03-12 14:05:02 +0200

    user profile page

--- app/src/main/java/com/example/foodshipping/Userprofile.java
+++ app/src/main/java/com/example/foodshipping/Userprofile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Logout extends AppCompatActivity {
+public class Userprofile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logout);
+        setContentView(R.layout.activity_userprofile);
     }
 }
